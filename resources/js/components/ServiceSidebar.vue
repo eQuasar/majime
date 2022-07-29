f37127176860afb016ca7e7107cf8f82899e1408
HS - Pup Dev <hsingh@equasar.com>
2022-07-29 12:02:47 +0530

    gitpush

--- resources/js/components/ServiceSidebar.vue
+++ resources/js/components/ServiceSidebar.vue
@@ -15,12 +15,6 @@ export default {
           href: { name: "admindashboard" },
           title: "Dashboard",
           icon: "fas fa-th",
-          child: [
-            {
-              href: { name: "adminperformance" },
-              title: "Performance",
-            },
-          ],
         },
         {
           title: "Sales",
